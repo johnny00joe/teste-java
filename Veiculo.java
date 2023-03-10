92592822caeedbe178425eda94c4c0df326e9afe
Johnny <61328423+johnny00joe@users.noreply.github.com>
2023-03-10 01:04:21 -0300

    tsete

--- Veiculo.java
+++ Veiculo.java
@@ -1,1 +1,25 @@
-//
+public class Veiculo {
+    Double velocidade;
+    String modelo;
+
+    public Carro(String modelo) {
+        this.modelo = modelo;
+        this.velocidade = 0;
+    }
+
+    public void acelerar() {
+        /* código do carro para acelerar */
+    }
+
+    public void frear() {
+        /* código do carro para frear */
+    }
+
+    public void acenderFarol() {
+        /* código do carro para acender o farol */
+    }
+
+    public static void main(String [] args) { 
+        System.out.println("Hello world"); 
+    } 
+}
